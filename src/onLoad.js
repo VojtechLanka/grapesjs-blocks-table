00f8ec9f7d6d0a7a30099a51d8fd6880133d275a
VojtechLanka <vojta.lanka@gmail.com>
2022-08-05 15:46:40 +0200

    do not repeat the background for column actions

--- src/onLoad.js
+++ src/onLoad.js
@@ -8,6 +8,7 @@ export default (editor, options = {}) => {
       .column-actions {
         background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="%23ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h3M3 21h3m0 0h4a2 2 0 0 0 2-2V9M6 21V9m0-6h4a2 2 0 0 1 2 2v4M6 3v6M3 9h3m0 0h6m-9 6h9m3-3h3m0 0h3m-3 0v3m0-3V9"/></svg>');
         background-size: cover;
+        background-repeat: no-repeat;
         content: '';
         background-size: 23px 23px;
         height: 23px;
